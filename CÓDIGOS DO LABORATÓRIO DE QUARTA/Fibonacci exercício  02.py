fbfd9b0fc36cb3cd2cc802b37a15fe03cc8dd8b0
Rian Kallebe da Silva Lisbôa <kallebelisboa@gmail.com>
2024-12-22 14:30:25 -0300

    exercícios de Fibonacci

---
+++ CÓDIGOS DO LABORATÓRIO DE QUARTA/Fibonacci exercício  02.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+count = 0
+count2 = 1
+
+fib = list()
+fib.append(count)
+fib.append(count2)
+
+while n > len(fib):
+    seq = count + count2
+    fib.append(seq)
+    if n > len(fib):
+        count = count2 + seq
+        fib.append(count)
+        if n > len(fib):
+            count2 = seq + count
+            fib.append(count2)
+print(*fib)
